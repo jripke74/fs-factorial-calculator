beee74232944fa7d0561a72a76787d13e9124ec9
Jeff Ripke <jripke74@gmail.com>
2025-03-10 11:15:38 -0700

    add for loo

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const num = 4;
 function factorialCalculator(number) {
-  const result = 1;
-  
+  let result = 1;
+  for (let i = 2; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 }
+
+const factorial = factorialCalculator(num);
+const resultMsg = `Factorial of ${num} is ${factorial}`;
+
+console.log(resultMsg);
